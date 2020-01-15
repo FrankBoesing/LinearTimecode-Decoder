74e1b67baa5eb95caf7d889a0f707975b656c4ed
Frank Bösing <f.boesing@gmx.de>
2020-01-16 00:08:19 +0100

    upload ltc-encode + decode example

---
+++ examples/ltc_gen/ltc_gen.ino
@@ -0,0 +1,196 @@
+/*
+  Linear Timecode generation and decoding combined
+
+  Please modify as needed
+
+  (c) Frank B, 2020
+  (pls keep copyright-info)
+
+  To enable decoding:
+  #define USE_LTC_INPUT
+  and connect pin1 to pinA2 (=pin 16)
+
+  Licence: MIT
+*/
+
+#define USE_LTC_INPUT //comment-out for generation only
+float fps = 30;
+const int ltcPin = 1;
+const int syncPin = 0;
+
+#include <TimeLib.h>
+
+
+#ifdef USE_LTC_INPUT
+/*
+   Connect ltc
+*/
+#include <Audio.h>
+#include <analyze_ltc.h>
+AudioInputAnalog         adc1;
+AudioAnalyzeLTC          ltc1;
+AudioConnection          patchCord2(adc1, 0, ltc1, 0);
+
+#else
+struct ltcframe_t {
+  uint64_t data;
+  uint16_t sync;
+};
+#endif
+
+IntervalTimer ltcTimer;
+IntervalTimer fpsTimer;
+
+volatile int clkCnt = 0;
+ltcframe_t ltc;
+float ltcTimer_freq;
+
+time_t getTeensy3Time()
+{
+  return Teensy3Clock.get();
+}
+
+void genLtc() {
+  static int state = 0;
+
+  if (clkCnt++ >= 2 * 80) {
+    //ltcTimer.end();
+    clkCnt = 0;
+    //Serial.println(".");
+    return;
+  }
+
+  int bitval;
+  int bitcount = clkCnt / 2;
+
+  ltcframe_t ltct;
+  ltct = ltc;
+
+  if (bitcount < 64) {
+    bitval = (int) (ltct.data >> bitcount) & 0x01;
+  } else {
+    bitval = (int) (ltct.sync >> (bitcount - 64)) & 0x01; //backward
+  }
+
+  if ( (bitval == 1) || ( (bitval == 0) && (clkCnt & 0x01) ) == 0) {
+    state = (state + 1) & 0x01; // toggle state
+    digitalWriteFast(ltcPin, state);
+  }
+
+  //if (clkCnt & 0x01) Serial.print(bitval);
+
+}
+
+//https://www.geeksforgeeks.org/program-to-find-parity/
+inline
+int getParity(uint64_t n)
+{
+  int parity = 0;
+  while (n)
+  {
+    parity = !parity;
+    n     = n & (n - 1);
+  }
+  return parity & 0x01;
+}
+
+
+/*
+
+   Gets called by rising edge of syncPin
+*/
+void startLtc() {
+
+  int t, t10;
+  uint64_t data = ltc.data;
+  clkCnt = 0;
+
+  //get frame number:
+  t = ((data >> 8) & 0x03) * 10 + (data & 0x0f);
+
+  //zero out ltc data, leave  d+c flags and user-bits untouched:
+  data &= 0xf0f0f0f0f0f0fcf0ULL;
+  //data = 0;
+  //inc frame-number:
+  //TODO: realize "drop frame numbering" (when D-Flag is set)
+  t++; 
+  if (t >= (int) fps) t = 0;
+  //set frame number:
+  t10 = t / 10;
+  data |= (t10 & 0x03) << 8;
+  data |= ((t - t10 * 10)) << 0;
+
+  //set time:
+  t = second();
+  t10 = t / 10;
+  data |= (t10 & 0x07) << 24; //Seconds tens
+  data |= ((t - t10 * 10) & 0x0f) << 16;
+
+  t = minute();
+  t10 = t / 10;
+  data |= (uint64_t)(t10 & 0x07) << 40; //minute tens
+  data |= (uint64_t)((t - t10 * 10) & 0x0f) << 32;
+
+  t = hour();
+  t10 = t / 10;
+  data |= (uint64_t)(t10 & 0x03) << 56; //hour tens
+  data |= (uint64_t)((t - t10 * 10) & 0x0f) << 48;
+
+  //set parity:
+  int parity = !getParity(data);
+  if ((int) fps == 25) {
+    data |= (uint64_t) parity << 59;
+  } else {
+    data |= (uint64_t) parity << 27;
+  }
+
+  ltc.data = data;
+
+  ltcTimer.begin(genLtc, ltcTimer_freq);
+  ltcTimer.priority(16);
+}
+
+void initLtcData()
+{
+  ltc.sync = 0xBFFC;
+  ltc.data = (uint64_t) 0; //<- place your initial data here
+}
+
+void genFpsSync() {
+  static int state = 0;
+  state = (state  + 1) & 0x01; // toggle state
+  digitalWriteFast(syncPin, state & 0x01);
+}
+
+void loop() {
+
+  while (1) {
+#ifdef USE_LTC_INPUT
+    if (ltc1.available()) {
+      ltcframe_t ltcframe = ltc1.read();
+      Serial.printf("%02d:%02d:%02d.%02d\n", ltc1.hour(&ltcframe), ltc1.minute(&ltcframe), ltc1.second(&ltcframe), ltc1.frame(&ltcframe));
+    }
+#else
+    Serial.printf(" %02d:%02d:%02d\n", hour(), minute(), second());
+    delay(1000);
+#endif
+  }
+}
+
+void setup() {
+#ifdef USE_LTC_INPUT
+  AudioMemory(4);
+#endif
+  setSyncProvider(getTeensy3Time);
+  pinMode(syncPin, OUTPUT);
+  pinMode(ltcPin, OUTPUT);
+  Serial.begin(9600);
+  initLtcData();
+  ltcTimer_freq = (1.0f / (2 * 80 * (fps ))) * 1000000.0f - 0.5f;// -1: make it a tiny bit faster than needed to allow syncing
+  fpsTimer.begin(&genFpsSync, (1.0f / (2 * fps)) * 1000000.0f);
+  fpsTimer.priority(32);
+  attachInterrupt(digitalPinToInterrupt(syncPin), &startLtc, RISING);
+
+  delay(1500);
+  Serial.println("Hello Tom");
+}
